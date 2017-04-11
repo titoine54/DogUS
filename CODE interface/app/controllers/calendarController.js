d44dfed0e02144003c17414bdf3daa7f5675bfae
titoine54 <titoine54@hotmail.com>
2017-04-10 22:32:21 -0400

    can only send a maximum of 6 events per day

--- CODE interface/app/controllers/calendarController.js
+++ CODE interface/app/controllers/calendarController.js
@@ -28,9 +28,10 @@ var calendarController = function (){
           const moment = require('moment');
 
           var message = "$,C," + collar_id + "," + day;
-          var message_filler = 7 - events.length;
+          var message_filler = 6 - events.length;
 
           for (i = 0; i < events.length; i++) {
+            if (i == 6){ break; }
             var tmp_time = moment(events[i].start_time, "hh:mm:ss a").toDate();
             var time = moment(tmp_time).format("H.mm");
             message = message + "," + time;
@@ -41,6 +42,7 @@ var calendarController = function (){
           }
 
           for (i = 0; i < events.length; i++) {
+            if (i == 6){ break; }
             var tmp_time = moment(events[i].end_time, "hh:mm:ss a").toDate();
             var time = moment(tmp_time).format("H.mm");
             message = message + "," + time;
